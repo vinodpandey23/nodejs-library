6fe0229771987bf20a5fa73b44bea411350f8897
Vinod Kumar Pandey <vinod@m-daq.com>
2025-01-16 17:40:30 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * Adds two numbers.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number} Sum of a and b
+ */
+function add(a, b) {
+    return a + b;
+}
+
+module.exports = {
+    add,
+};
